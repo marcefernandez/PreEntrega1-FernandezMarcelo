81274b0c776d31abc9c99def7d115bf9684a26b9
marcefernandez <sebastianmf2014@hotmail.com>
2023-02-15 01:43:44 -0300

    agregué NavBar, falta CartWidget

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styleNav.css';
+
+export const NavBar = () => {
+    return (
+        
+            <nav className='nav'>
+                <div className='nav_logo'>
+                    <img src='https://i.pinimg.com/564x/75/b7/f2/75b7f20bb508a50d668c3c17801fc1fc.jpg' alt='logo'/>
+                </div>
+                <div className='nav_title'>
+                    <h1>MotoSafe</h1>
+                </div>
+            </nav>
+        
+
+    );
+}
+
+export default NavBar;
